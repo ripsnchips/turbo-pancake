5d67cb2a3033f4d55df5add87bcfc7b2d90f0db8
tnklassen <tklassen@asu.edu>
2018-12-09 15:16:59 -0500

    print addition yo but A DIFFERENT ONE FROM THE FIRST TWO

--- src/main/java/Klass.java
+++ src/main/java/Klass.java
@@ -1,5 +1,6 @@
 public class Klass {
     public static void main(String args[]){
         System.out.println("please tell me maven decided to get her shit together this time");
+        System.out.println("let's get physical\n\nPHYSICAL");
     }
 }
